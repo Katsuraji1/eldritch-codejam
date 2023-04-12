3710ae880de69ea9d9a0fb0ed6e2e2d4dc7aad8d
Katsuraji1 <verahovets@gmail.com>
2023-04-12 12:49:08 +0300

    fix

--- data/mythicCards/index.js
+++
@@ -1,9 +0,0 @@
-import brownCards from './brown/index.js';
-import blueCards from './blue/index.js';
-import greenCards from './green/index.js';
-
-export {
-  brownCards,
-  blueCards,
-  greenCards
-}
